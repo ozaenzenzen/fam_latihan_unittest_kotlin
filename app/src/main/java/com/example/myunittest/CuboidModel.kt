c1a60ce7f45861938a8e52c5473e1aef4fa853d3
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-11 07:32:09 +0700

    update logic cuboid

--- app/src/main/java/com/example/myunittest/CuboidModel.kt
+++ app/src/main/java/com/example/myunittest/CuboidModel.kt
@@ -8,10 +8,9 @@ class CuboidModel {
     fun getVolume(): Double = width * length * height
 
     fun getSurfaceArea(): Double {
-        val wl = width + length
-        val wh = width + height
-        val lh = length + height
-
+        val wl = width * length
+        val wh = width * height
+        val lh = length * height
         return 2 * (wl + wh + lh)
     }
 
